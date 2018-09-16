6d4c739e4f70a4568d2516f3303e75e24831a46a
olvfg <gbfolv@gmail.com>
2018-09-16 14:15:16 -0300

    Primeiro Commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="post-list">
+    <div
+      v-for="post in postList"
+      :key="post._id"
+    >
+      <b-card>
+        <b-media>
+          <b-img slot="aside" blank blank-color="#ccc" width="64" alt="placeholder" />
+          <h5 class="mt-0" v-text="post.user"></h5>
+          <p v-text="post.content"></p>
+        </b-media>
+        <div class="btn-card">
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Remover</b-button>
+        </div>
+      </b-card>
+    </div>
+
+    <!-- <div
+      class="post-box"
+      v-for="post in postList"
+      :key="post._id"
+    >
+      <p class="post-box-content" v-text="post.content"></p>
+
+      <div class="post-session">
+        <div class="post-box-user">
+          <span v-text="post.user"></span>
+        </div>
+        <el-button
+            class="remove-post"
+            type="primary"
+            @click="removePost(post)"
+        >Remover</el-button>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import { mapGetters, mapActions } from 'vuex'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+
+  name: 'posts',
+
+  computed: {
+      ...mapGetters([ 'getPostList' ]),
+
+      postList () {
+          let list = this.getPostList
+          return list.reverse()
+      }
+  }
+
+}
+
+</script>
---
+++ src/store/store.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Api from '../api/api'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    posts: []
+  },
+
+  actions: {
+    loadPostList: function ({ commit }) {
+      Api().get('/posts')
+        .then((response) => {
+          commit('SET_POST_LIST', { list: response.data }, (err) => {
+            console.log(err)
+          })
+        })
+    },
+
+    async createPost ({ state, dispatch, commit }, newPostInfo) {
+      await Api().post('posts', { user: newPostInfo.user, title: 'title', content: newPostInfo.content })
+    },
+
+    async deletePost ({ state, dispatch, commit }, post) {
+      await Api().delete(`posts/${post._id}`)
+
+      return dispatch('loadPostList', { commit })
+    }
+
+  },
+
+  mutations: {
+
+    SET_POST_LIST: (state, { list }) => {
+      state.posts = list.data
+    }
+  },
+
+  getters: {
+    getPostList: state => state.posts
+  }
+
+})
+
+export default store
